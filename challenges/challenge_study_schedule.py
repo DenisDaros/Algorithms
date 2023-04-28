17409c623f8ac0310af1407ec168105e413ddeaa
denis daros <denisv.daros@gmail.com>
2023-04-28 18:12:44 -0300

    criando contador study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    count = 0
+    try:
+        for i, j in permanence_period:
+            if i <= target_time <= j:
+                count += 1
+    except (ValueError, TypeError, AssertionError):
+        return None
+    return count
