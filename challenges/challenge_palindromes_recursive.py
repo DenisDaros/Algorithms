52db3b377b68e67e6d42fd307832d0848c870b3f
denis daros <denisv.daros@gmail.com>
2023-04-29 16:16:26 -0300

    aplicando recursividade na função ispalindrome

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word or word == '':
+        return False
+    if word[low_index] != word[high_index]:
+        return False
+
+    if high_index <= low_index:
+        return True
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+
+teste = is_palindrome_recursive("daad", 0, -1)
+print(teste)
